70083541fb74bcfb9a646442d8823c9957508421
Bao <2421443120@qq.com>
2022-04-22 14:13:32 +0800

    update

--- src/components/home/ObtainFund.vue
+++ src/components/home/ObtainFund.vue
@@ -1,41 +1,54 @@
 <template>
-  <div id="ObtainFund"
-       style="height: 100%; width: 100%">
-    <el-card class="box-card"
-             style="
+  <div id="ObtainFund" style="height: 100%; width: 100%">
+    <el-card
+      class="box-card"
+      style="
         height: calc(100% - 40px);
         height: calc(100% - 40px);
         margin: 20px;
         padding: 30px;
       "
-             shadow="always">
+      shadow="always"
+    >
       <div>
         <el-row>
-          <p style="font-size:30px">持有产品列表</p>
+          <p style="font-size: 30px">持有产品</p>
         </el-row>
         <el-row>
           <el-col>
             <template>
-              <el-table :data="ObtainData"
-                        style="width: 100%"
-                        :header-cell-style="headStyle"
-                        :cell-style="rowStyle">
-                <el-table-column label="产品代码"
-                                 min-width="200px">
+              <el-table
+                :data="fundList"
+                style="width: 100%"
+                :header-cell-style="headStyle"
+                :cell-style="rowStyle"
+              >
+                <el-table-column label="产品代码" min-width="200px">
                   <template slot-scope="scope">
-                    <span>{{ scope.row.pro_id}}</span>
+                    <span>{{ scope.row.prdct_id }}</span>
                   </template>
                 </el-table-column>
-                <el-table-column label="客户编号"
-                                 min-width="200px">
+                <el-table-column label="产品名称" min-width="200px">
                   <template slot-scope="scope">
-                    <span>{{ scope.row.cstmr_id}}</span>
+                    <span>{{ scope.row.prdct_name }}</span>
                   </template>
                 </el-table-column>
-                <el-table-column label="持有份额"
-                                 min-width="200px">
+                <el-table-column label="持有份额" min-width="200px">
                   <template slot-scope="scope">
-                    <span>{{ scope.row.shares}}</span>
+                    <span>{{ scope.row.prdct_shares.toFixed(2) }}</span>
+                  </template>
+                </el-table-column>
+                <el-table-column label="操作" min-width="200px">
+                  <template slot-scope="scope">
+                    <el-button
+                      size="mini"
+                      @click="
+                        dialogFormVisible = true
+                        fundRedemption(scope.row.prdct_id, scope.row.shares)
+                      "
+                    >
+                      赎回
+                    </el-button>
                   </template>
                 </el-table-column>
               </el-table>
@@ -44,29 +57,75 @@
         </el-row>
       </div>
     </el-card>
+    <el-dialog title="赎回" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="产品名称：">
+          <span>{{ prdct_name }}</span>
+        </el-form-item>
+        <el-form-item label="持有份额：">
+          <span>{{ prdct_shares.toFixed(2) }}</span>
+        </el-form-item>
+        <el-form-item label="赎回份额" :inline="true">
+          <el-input placeholder="赎回份额" v-model="input"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ObtainFund',
+  created () {
+    this.getFundOwnList()
+  },
   data () {
     return {
-      ObtainData: [
-        {
-          pro_id: 123,
-          cstmr_id: 12,
-          shares: 99
-        }
-      ]
+      dialogFormVisible: false,
+      input: '',
+      fundList: []
     }
   },
   methods: {
+    getFundOwnList () {
+      console.log(this.$store.state.user.user_id)
+      this.$axios
+        .get('/api/user/own', {
+          params: {
+            cstmr_id: this.$store.state.user.user_id
+          }
+        })
+        .then((res) => {
+          console.log(res.data)
+          this.fundList = res.data.product_info
+          console.log(this.fundList)
+        })
+        .catch((failResponse) => {})
+    },
     headStyle () {
       return 'text-align:center'
     },
     rowStyle () {
       return 'text-align:center'
+    },
+    fundRedemption (prdctId, shares) {
+      this.dialogFormVisible = true
+      // this.$axios
+      //   .get('/api/trade/sale', {
+      //     params: {
+      //       prdct_id: prdctId,
+      //       cstmr_id: this.$store.state.user.user_id,
+      //       trans_share: shares
+      //     }
+      //   })
+      //   .then((res) => {
+      //     console.log(res.data)
+      //   })
+      //   .catch((failResponse) => {})
     }
   }
 }
--- src/components/home/createCustomerInformation.vue
+++ src/components/home/createCustomerInformation.vue
@@ -17,18 +17,30 @@
         <el-input v-model="ruleForm.name" placeholder="点击输入姓名"></el-input>
       </el-form-item>
       <el-form-item label="证件类型" prop="credentialsType">
-        <el-select v-model="ruleForm.credentialsType" placeholder="请选择证件类型">
+        <el-select
+          v-model="ruleForm.credentialsType"
+          placeholder="请选择证件类型"
+        >
           <el-option label="身份证" value="IdentityCard"></el-option>
           <el-option label="护照" value="Passport"></el-option>
           <el-option label="港澳台居住证" value="HMT"></el-option>
-          <el-option label="组织机构代码证" value="OrganizationCode"></el-option>
+          <el-option
+            label="组织机构代码证"
+            value="OrganizationCode"
+          ></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="证件号码" prop="credentialsNum">
-        <el-input v-model="ruleForm.credentialsNum" placeholder="点击输入证件号码"></el-input>
+        <el-input
+          v-model="ruleForm.credentialsNum"
+          placeholder="点击输入证件号码"
+        ></el-input>
       </el-form-item>
       <el-form-item label="银行卡卡号" prop="cardId">
-        <el-input v-model="ruleForm.cardId" placeholder="点击输入本人的银行卡号"></el-input>
+        <el-input
+          v-model="ruleForm.cardId"
+          placeholder="点击输入本人的银行卡号"
+        ></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="submitForm('ruleForm')"
@@ -67,7 +79,12 @@ export default {
         ],
         credentialsNum: [
           { required: true, message: '请输入证件号', trigger: 'blur' },
-          { min: 18, max: 18, message: '身份证号码长度必须是18位', trigger: 'blur' }
+          {
+            min: 18,
+            max: 18,
+            message: '身份证号码长度必须是18位',
+            trigger: 'blur'
+          }
         ],
         cardId: [
           { required: true, message: '请输入银行卡卡号添加', trigger: 'blur' }
